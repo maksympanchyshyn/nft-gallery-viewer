183adbbb489dd0c0e6c6c8df48038ef27ffade01
Maksym Panchyshyn <maksymm.panchyshyn@gmail.com>
2022-08-08 14:45:19 +0300

    Add header. Add ability to connect wallet

---
+++ components/Header/index.tsx
@@ -0,0 +1,52 @@
+import { ethers } from 'ethers';
+import { useEffect, useState } from 'react';
+
+import { Balance, ConnectBtn, Container, Logo, AccountContainer } from './styled';
+
+const Header = () => {
+  const [isConnected, setIsConnected] = useState(false);
+  const [accountBalance, setAccountBalance] = useState('');
+  const [accountAddress, setAccountAddress] = useState('');
+
+  useEffect(() => {
+    const checkConnection = async () => {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const accounts = await provider.send('eth_accounts', []);
+
+      if (accounts.length > 0) {
+        const balance = await provider.getBalance(accounts[0]);
+        setAccountAddress(accounts[0]);
+        setAccountBalance(ethers.utils.formatEther(balance));
+        setIsConnected(true);
+      }
+    };
+    checkConnection();
+  }, []);
+
+  const connectMetamask = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const accounts = await provider.send('eth_requestAccounts', []);
+    const balance = await provider.getBalance(accounts[0]);
+
+    setAccountAddress(accounts[0]);
+    setAccountBalance(ethers.utils.formatEther(balance));
+    setIsConnected(true);
+  };
+
+  return (
+    <Container>
+      <Logo>Logo placeholder</Logo>
+      <AccountContainer>
+        {isConnected ? (
+          <Balance>
+            {`${accountAddress.slice(0, 6)}...${accountAddress.slice(0, 6)}`} - {accountBalance.slice(0, 5)} ETH
+          </Balance>
+        ) : (
+          <ConnectBtn onClick={connectMetamask}>Connect Wallet</ConnectBtn>
+        )}
+      </AccountContainer>
+    </Container>
+  );
+};
+
+export default Header;
---
+++ components/Header/styled.ts
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 72px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Logo = styled.div`
+  color: #fff;
+  font-size: 24px;
+`;
+
+export const AccountContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ConnectBtn = styled.button`
+  cursor: pointer;
+  padding: 8px 12px;
+  background: rgb(117, 90, 250);
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  font-size: 16px;
+`;
+
+export const Balance = styled.div`
+  padding: 8px 12px;
+  background: rgb(117, 90, 250);
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  font-size: 16px;
+`;
